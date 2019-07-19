5b740b1930f27e1114b479f1587b0f02fea2ddc3
Nate Walczak <nate.walczak@detroitlabs.com>
2019-07-19 17:28:15 -0400

    Update setup script

--- setup.swift
+++ setup.swift
@@ -2,49 +2,17 @@
 
 import Foundation
 
-protocol FileProvider {
-    
-    var name: String { get }
-    
-    func writeToDisk()
-    
+let arguments = CommandLine.arguments
+
+let targetRootPath = FileManager.default.currentDirectoryPath
+
+let sourceRootPath: String
+if arguments.contains("--local") {
+    sourceRootPath = ("file://\(targetRootPath)/\(arguments[0])" as NSString).deletingLastPathComponent
+} else {
+    // TODO: replace `extract-files` with `master`
+    sourceRootPath = "https://raw.githubusercontent.com/detroit-labs/fastlane-template/extract-files"
 }
-
-/// A file to be written to the current directory under `name` containing `contents`.
-struct File: FileProvider {
-    
-    let name: String
-    let contents: Data
-    
-    func writeToDisk() {
-        let currentPath = FileManager.default.currentDirectoryPath
-        
-        if name.contains("/") {
-            let directoryPath = ("\(currentPath)/\(name)" as NSString).deletingLastPathComponent
-            try! FileManager.default.createDirectory(atPath: directoryPath, withIntermediateDirectories: true)
-        }
-        
-        FileManager.default.createFile(atPath: "\(currentPath)/\(name)", contents: contents)
-    }
-    
-}
-
-
-/// A file to be downloaded from `url` and saved to the current directory as `name`.
-struct RemoteFile: FileProvider {
-    
-    let url: URL
-    let name: String
-    
-    func writeToDisk() {
-        let process = Process.launchedProcess(launchPath: "/usr/bin/curl", arguments: ["-s", "--create-dirs", "-o", "\((FileManager.default.currentDirectoryPath as NSString).appendingPathComponent(name))", "\(url.absoluteString)"])
-        process.waitUntilExit()
-    }
-    
-}
-
-// When we get downloadable templates, put them here.
-// let prTemplate = RemoteFile(url: URL(string: "https://raw.githubusercontent.com/detroit-labs/fastlane-template/master/fastlane/Fastfile")!, name: "github/Fastfile")
 
 let projectFileName: String = {
     let currentPath = FileManager.default.currentDirectoryPath
@@ -52,75 +20,62 @@ let projectFileName: String = {
     let projects = contents.filter { $0.contains("xcodeproj") }
     guard projects.count == 1,
         let project = projects.first else {
-        print("Either zero or too many xcodeprojs found, run this script on new projects only.")
-        exit(0)
+            print("Either zero or too many xcodeprojs found, run this script on new projects only.")
+            exit(0)
     }
-
+    
     return project
 }()
 
-let projectName = projectFileName.components(separatedBy: ".")[0]
+let projectName = (projectFileName as NSString).deletingPathExtension
+let workspaceName = (projectName as NSString).appendingPathExtension("xcworkspace")!
 
-let rubyVersion: File = {
-    let rubyVersionContent = "2.6.3\n".data(using: .utf8)!
+let environment: [String: String] = [
+    "RUBY_GEMSET": projectName.lowercased(),
+    "RUBY_VERSION": "2.6.3",
+    "XCODE_SCHEME_NAME": projectName,
+    "XCODE_WORKSPACE_NAME": workspaceName,
+    "XCODE_VERSION": "10.2",
+]
 
-    return File(name: ".ruby-version", contents: rubyVersionContent)
-}()
-
-let rubyGemset: File = {
-    let rubyGemsetContent = "\(projectName.lowercased())\n".data(using: .utf8)!
-
-    return File(name: ".ruby-gemset", contents: rubyGemsetContent)
-}()
-
-let gemfile: File = {
-    let gemfileContent = """
-                        source \"https://rubygems.org\"
-
-                        gem \"cocoapods\"
-                        gem \"fastlane\"
-                        gem \"xcode-install\"
-
-                        """.data(using: .utf8)!
-
-    return File(name: "Gemfile", contents: gemfileContent)
-}()
-
-let fastfile: File = {
-    let fastfileContent = """
-                        default_platform :ios
-
-                        import_from_git(url: 'https://github.com/detroit-labs/fastlane-template')
-
-                        """.data(using: .utf8)!
-
-    return File(name: "fastlane/Fastfile", contents: fastfileContent)
-}()
-
-let probableWorkspaceName = projectFileName.replacingOccurrences(of: "xcodeproj", with: "xcworkspace")
-let probableSchemeName = projectName
-
-func env(withWorkspaceName workspaceName: String, schemeName: String) -> File {
-    let simSetting = "FASTLANE_EXPLICIT_OPEN_SIMULATOR=1"
-    let gymWorkspace = "GYM_WORKSPACE=\"\(workspaceName)\""
-    let gymScheme = "GYM_SCHEME=\"\(schemeName)\""
-    let gymClean = "GYM_CLEAN=true"
-    let scanWorkspace = "SCAN_WORKSPACE=\"\(workspaceName)\""
-    let scanScheme = "SCAN_SCHEME=\"\(schemeName)\""
-    let scanClean = "SCAN_CLEAN=true"
-    let profileDirectory = "PROFILE_DIRECTORY=\"../Profiles\""
-    let xcodeVersion = "XCODE_VERSION=\"~> 10.2\""
-
-    let env = [simSetting, gymWorkspace, gymScheme, gymClean, scanWorkspace, scanScheme, scanClean, profileDirectory, xcodeVersion].joined(separator: "\n") + "\n"
-    let envContent = env.data(using: .utf8)!
-
-    return File(name: "fastlane/.env", contents: envContent)
+func expand(_ contents: String) -> String {
+    var contents = contents
+    environment.keys.forEach { key in
+        contents = contents.replacingOccurrences(of: "__\(key)__", with: environment[key]!)
+    }
+    return contents
 }
 
-let files: [FileProvider] = [rubyVersion, rubyGemset, gemfile, fastfile, env(withWorkspaceName: probableWorkspaceName, schemeName: probableSchemeName)]
+struct TemplateFile {
+    
+    let source: String
+    let target: String
+    
+    func writeToDisk() {
+        if target.contains("/") {
+            let directoryPath = ("\(targetRootPath)/\(target)" as NSString).deletingLastPathComponent
+            try! FileManager.default.createDirectory(atPath: directoryPath, withIntermediateDirectories: true)
+        }
+        
+        let sourceURL = URL(string: "\(sourceRootPath)/templates/\(source)")!
+        
+        let contents = try! String(contentsOf: sourceURL, encoding: .utf8)
+        let data = expand(contents).data(using: .utf8)!
+        FileManager.default.createFile(atPath: "\(targetRootPath)/\(target)", contents: data)
+    }
+    
+}
+
+let files = [
+    TemplateFile(source: "fastlane/env", target: "fastlane/.env"),
+    TemplateFile(source: "fastlane/Fastfile", target: "fastlane/Fastfile"),
+    TemplateFile(source: "gems/Gemfile", target: "Gemfile"),
+    TemplateFile(source: "ruby/ruby-gemset", target: ".ruby-gemset"),
+    TemplateFile(source: "ruby/ruby-version", target: ".ruby-version")
+]
+
 files.forEach { file in
     file.writeToDisk()
-    print("\(file.name) written to disk.")
+    print("\(file.target) written to disk.")
 }
 print("Files written.")
-
